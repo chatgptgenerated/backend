c93951afdb302ba4b58588460a871851da8427b9
Alexandru Miclea <alexandrumiclea@outlook.com>
2024-12-07 19:12:08 +0200

    added database connection

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,6 +1,7 @@
-using Microsoft.AspNetCore.Mvc;
-
 namespace backend.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using backend.Models;
 
 [ApiController]
 [Route("[controller]")]
---
+++ Models/Employee.cs
@@ -0,0 +1,7 @@
+namespace backend.Models{
+    public class Employee {
+        public int Id {get; set;}
+        public int Name {get; set;}
+        public int Title {get; set;}
+    }
+}
